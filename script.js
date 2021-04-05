dd42f9241dc0615bb46a56b68da89743cddfd36e
Mateus Henrique <mhenriqq32@gmail.com>
2021-04-05 19:43:03 -0300

    updating encurtador de link

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('label').addEventListener(('click'), () =>{
+  document.querySelector('.menu nav').classList.toggle('ativo')
+  document.querySelector('.login-field').classList.toggle('ativo')
+  document.querySelector('.header').classList.toggle('ativo')
+})
